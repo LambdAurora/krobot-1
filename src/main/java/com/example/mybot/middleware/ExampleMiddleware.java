036be49abef0bcd0a7b93c53dc6453f6926dbeb8
Adrien Navratil <adrien1975@live.fr>
2017-04-11 01:55:02 +0200

    Fix

--- src/main/java/com/example/mybot/middleware/ExampleMiddleware.java
+++ src/main/java/com/example/mybot/middleware/ExampleMiddleware.java
@@ -3,12 +3,14 @@ package com.example.mybot.middleware;
 import fr.litarvan.krobot.command.Command;
 import fr.litarvan.krobot.command.CommandContext;
 import fr.litarvan.krobot.command.Middleware;
+import fr.litarvan.krobot.command.SuppliedArgument;
 import fr.litarvan.krobot.util.Dialog;
+import java.util.Map;
 
 public class ExampleMiddleware implements Middleware
 {
     @Override
-    public boolean handle(Command command, CommandContext context)
+    public boolean handle(Command command, Map<String, SuppliedArgument> args, CommandContext context)
     {
         // Will be called before every command
 
